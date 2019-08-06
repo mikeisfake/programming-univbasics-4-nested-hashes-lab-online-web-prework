07a262d0e059ab203753e856d58baba272b0b277
Mike Cooper <mikescooper@icloud.com>
2019-08-06 19:57:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_richie][:languages].fetch(0)
+     programmer_hash[:dennis_richie][:languages].join
 end
 
 def adding_matz
